9fde6d0e1abf0f4a6b3f3dfbbb696d15a35b479b
elise <elise@elise-desktop>
2012-11-27 06:31:07 +0900

    python script

---
+++ other/expat.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python
+# coding: UTF-8
+import re, os, sys, json
+from xml.etree import ElementTree
+from xml.parsers import expat
+
+class Element(object):
+    def __init__(self, name, attributes):
+        self.name = name
+        self.attributes = attributes
+        self.cdata = ''
+        self.children = []
+    def addChild(self, element):
+        self.children.append(element)
+    def getAttribute(self, key):
+        return self.attributes.get(key)
+    def getData(self):
+        return self.cdata
+    def getElements(self, name=""):
+        if name:
+            return [c for c in self.children if c.name == name]
+        else:
+            return list(self.children)
+
+class Xml2Obj(object):
+    def __init__(self):
+        self.root = None
+        self.nodeStack = []
+    def StartElement(self, name, attributes):
+        element = Element(name.encode(), attributes)
+        if self.nodeStack:
+            parent = self.nodeStack[-1]
+            parent.addChild(element)
+        else:
+            self.root = element
+        self.nodeStack.append(element)
+    
+    def EndElement(self, name):
+        self.nodeStack.pop()
+    def CharacterData(self, data):
+        if data.strip():
+            data = data.encode()
+            element = self.nodeStack[-1]
+            element.cdata += data
+    def Parse(self, filename):
+        Parser = expat.ParserCreate()
+        Parser.StartElementHandler = self.StartElement
+        Parser.EndElementHandler = self.EndElement
+        Parser.CharacterDataHandler = self.CharacterData
+        
+        ParserStatus = Parser.Parse(open(filename).read(), 1)
+        return self.root
+
+
+if __name__ == "__main__"  :
+    print "start"
+    parser = Xml2Obj()
+    root_element = parser.Parse('sample.xml')
+    print "end"
+
